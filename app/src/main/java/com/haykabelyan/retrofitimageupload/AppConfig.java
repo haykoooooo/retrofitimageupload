17f741c4a9ccdc452f6b621789d1ae58d7b4208b
Hayk <abelyanhayk@yahoo.com>
2018-01-18 16:24:46 +0400

    Image upload and fullsize view

---
+++ app/src/main/java/com/haykabelyan/retrofitimageupload/AppConfig.java
@@ -0,0 +1,14 @@
+package com.haykabelyan.retrofitimageupload;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class AppConfig {
+
+    private static String BASE_URL = "http://myandroid.000webhostapp.com/";
+
+    static Retrofit getRetrofit() {
+
+        return new Retrofit.Builder().baseUrl(AppConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+    }
+}
---
+++ app/src/main/java/com/haykabelyan/retrofitimageupload/MainActivity.java
@@ -0,0 +1,147 @@
+package com.haykabelyan.retrofitimageupload;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    String mediaPath;
+    ImageView imageView, fullImageView;
+    ScrollView imageViewView;
+    String[] mediaColumns = {MediaStore.Video.Media._ID};
+    ProgressDialog progressDialog;
+    Bitmap bitmap;
+    Cursor cursor;
+    boolean isFullImageViewShown;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading...");
+        progressDialog.setCancelable(false);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageViewView = (ScrollView) findViewById(R.id.imageViewView);
+        fullImageView = (ImageView) findViewById(R.id.fullImageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == 0 && resultCode == RESULT_OK && null != data) {
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                assert cursor != null;
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                mediaPath = cursor.getString(columnIndex);
+                bitmap = BitmapFactory.decodeFile(mediaPath);
+                uploadFile();
+            } else {
+                Toast.makeText(this, "You haven't picked Image/Video", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public Bitmap getThumbnailPathForLocalFile(Activity context, Uri fileUri) {
+        long fileId = getFileId(context, fileUri);
+        return MediaStore.Video.Thumbnails.getThumbnail(context.getContentResolver(), fileId, MediaStore.Video.Thumbnails.MICRO_KIND, null);
+    }
+
+    public long getFileId(Activity context, Uri fileUri) {
+        Cursor cursor = context.managedQuery(fileUri, mediaColumns, null, null, null);
+        if (cursor.moveToFirst()) {
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+            return cursor.getInt(columnIndex);
+        }
+        return 0;
+    }
+
+    private void uploadFile() {
+        progressDialog.show();
+        // Map is used to multipart the file using okhttp3.RequestBody
+        File file = new File(mediaPath);
+        // Parsing any Media type file
+        RequestBody requestBody = RequestBody.create(MediaType.parse("*/*"), file);
+        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
+        RequestBody filename = RequestBody.create(MediaType.parse("text/plain"), file.getName());
+        ApiConfig getResponse = AppConfig.getRetrofit().create(ApiConfig.class);
+        Call<ServerResponse> call = getResponse.uploadFile(fileToUpload, filename);
+        call.enqueue(new Callback<ServerResponse>() {
+            @Override
+            public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+                ServerResponse serverResponse = response.body();
+                if (serverResponse != null) {
+                    if (serverResponse.getSuccess()) {
+                        Toast.makeText(getApplicationContext(), serverResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                        imageView.setImageBitmap(bitmap);
+                        imageView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                imageViewView.setVisibility(View.VISIBLE);
+                                isFullImageViewShown = true;
+                                fullImageView.setImageBitmap(bitmap);
+                            }
+                        });
+                        cursor.close();
+                    } else {
+                        Toast.makeText(getApplicationContext(), serverResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    assert serverResponse != null;
+                    Log.v("Response", serverResponse.toString());
+                }
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<ServerResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isFullImageViewShown) {
+            imageViewView.setVisibility(View.GONE);
+            isFullImageViewShown = false;
+        } else moveTaskToBack(true);
+    }
+}
